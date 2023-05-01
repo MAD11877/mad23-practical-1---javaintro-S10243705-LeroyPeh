129524bb7e1c1f652a109cd18e5c2aa99187034f
S10243705-LeroyPeh <130967155+S10243705-LeroyPeh@users.noreply.github.com>
2023-05-01 14:46:08 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,8 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int integer = in.nextInt();
+    int multi = integer*integer;
+    System.out.println(multi);
   }
 }
